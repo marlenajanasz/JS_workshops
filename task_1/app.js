98ddfda9899c8fbe5e9e2e3a8cc57da26fe59575
Marlena Janasz <mar.janasz87@gmail.com>
2020-02-02 19:48:53 +0100

    Update app.js

--- task_1/app.js
+++ task_1/app.js
@@ -1,29 +1,10 @@
-const inputTop = document.getElementById("top");
-const inputBottom = document.getElementById("bottom");
+let inputTop = document.getElementById("top");
+let inputBottom = document.getElementById("bottom");
 
-/*const nameTop = inputTop.getAttribute("name");
-const nameBottom = inputBottom.getAttribute("name");*/
+function changeColor(){
 
-const valueTop = inputTop.getAttribute("value");
-const valueBottom = inputBottom.getAttribute("value");
-
-funkcja1(bottom, top){
-
-  if(bottom !== '') && (top !==''){  
-  		document.body.background = linear-gradient(bottom, top);
-	} 
+    document.body.style.background = `linear-gradient(${inputTop.value}, ${inputBottom.value})`;
+	 
 }
 
-funkcja2(){
-
-
-  funkcja1(valueBottom, valueTop);
-
-} 
-
-
-
-document.getElementById("top").onchange = funkcja1; //
-document.getElementById("bottom").onchange = funkcja1; //ta funcja ma zrobić że jeśli wybierzesz dany kolor to zmienia się tło
-
-document.getElementById("btn").onclick = funkcja2; //ta funkcja ma uruchomić funkcję wyżej
+document.getElementById("btn").onclick = changeColor;
